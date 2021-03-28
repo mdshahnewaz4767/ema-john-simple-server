b63bdeffd01973c76d9c357515ff91d175db8fb3
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-28 21:17:53 +0600

    Load some product using keys

--- index.js
+++ index.js
@@ -19,12 +19,43 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const productsCollection = client.db("emaJohnStore").collection("products");
   
+  //demo 
+  app.get('/', (req, res) => {
+    res.send('Hello ema john!')
+  })
+
+  //create data
   app.post('/addProduct', (req, res) => {
-      const product = req.body;
-      productsCollection.insertOne(product)
+      const products = req.body;
+      // console.log(products);
+      productsCollection.insertMany(products)
       .then(result => {
-          console.log(result);
+          console.log(result.insertedCount);
+          res.send(result.insertedCount)
       })
+  })
+
+  //read data
+  app.get('/products', (req, res) => {
+    productsCollection.find({}).limit(20)
+    .toArray( (err, documents) => {
+      res.send(documents);
+    })
+  })
+  //read single product
+  app.get('/product/:key', (req, res) => {
+    productsCollection.find({key: req.params.key}).limit(20)
+    .toArray( (err, documents) => {
+      res.send(documents[0]);
+    })
+  })
+  //read multiple keys
+  app.post('/productsByKeys', (req, res) => {
+    const productKeys = req.body;
+    productsCollection.find({key: { $in: productKeys } })
+    .toArray( (err, documents) => {
+      res.send(documents);
+    })
   })
 });
 
